0b85dccec66c4dfffc1830bd39fbbcd9067fe486
Chuck Grindel <chuck.grindel@gmail.com>
2024-04-08 13:44:57 -0600

    Add debug code.

--- tools/check_jobs.sh
+++ tools/check_jobs.sh
@@ -70,6 +70,10 @@ done
 api_url="/repos/${github_repository}/actions/runs/${github_run_id}/attempts/${github_run_attempt}/jobs"
 jobs_json="$( gh api --paginate "${api_url}" )"
 
+# DEBUG BEGIN
+echo >&2 "*** CHUCK $(basename "${BASH_SOURCE[0]}") jobs_json: ${jobs_json}" 
+# DEBUG END
+
 # Retrieve the jobs of interest.
 filter_jobs_cmd=( get_filtered_jobs_json --current_job "${github_job}" --job_json "${jobs_json}" )
 [[ -n "${job_names:-}" ]] && filter_jobs_cmd+=( --job_names "${job_names}" )
